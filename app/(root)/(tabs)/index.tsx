a93fc24859018abeec8b1a9a6481a05f6e3b65f7
Kelvin Brito <britok30@gmail.com>
2025-01-06 15:17:52 -0500

    fonts and themes

--- app/(root)/(tabs)/index.tsx
+++ app/(root)/(tabs)/index.tsx
@@ -3,13 +3,8 @@ import { Text, View } from "react-native";
 
 export default function Index() {
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
+    <View className="flex-1 justify-center items-center">
+      <Text className="font-bold my-10 font-rubik text-3xl">Welcome to ReState</Text>
       <Link href="/sign-in">Sign In</Link>
       <Link href="/explore">Explore</Link>
       <Link href="/profile">Profile</Link>
